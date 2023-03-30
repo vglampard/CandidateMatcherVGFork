2de655ed9686c611302878290928b36ec2f255f9
btxoana <beatrice.stanila@gmail.com>
2023-03-30 13:31:11 +0100

    Drop down input not completed yet

--- jobsearch/src/components/Candidate/Candidate.js
+++ jobsearch/src/components/Candidate/Candidate.js
@@ -1,12 +1,16 @@
-import React from 'react'
-
-export default function Candidate({name, email, skills, availability}) {
+import React from "react";
+export default function Candidate({ name, email, skills, availability }) {
   return (
-    <div className=" flex gap-3 bg-green-100 m-2" >
-        <p>Name: {name}</p>
-        <p>Contact: {email}</p>
-        <div className="flex gap-2 text-sm"><p>Skills: </p>{skills.map((skill)=> <p>{skill}</p>)}</div>
-        <p>Availability: {availability}</p>
+    <div className=" flex gap-3 bg-green-100 m-2">
+      <p>Name: {name}</p>
+      <p>Contact: {email}</p>
+      <div className="flex gap-2 text-sm">
+        <p>Skills: </p>
+        {skills.map((skill) => (
+          <p>{skill}</p>
+        ))}
+      </div>
+      <p>Availability: {availability}</p>
     </div>
-  )
+  );
 }
--- jobsearch/src/components/Input/Input.js
+++ jobsearch/src/components/Input/Input.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function Input() {
+export default function Input({ requiredSkills, setRequiredSkills }) {
+  function eventHandler(e) {
+    setRequiredSkills([...requiredSkills, e.target.value]);
+  }
   return (
-    <div className="w-1/3 bg-green-100 m-2">
-        FORM
-    </div>
-  )
+    <form className="w-1/3 bg-green-100 m-2 flex-1">
+      <label for="skills">Choose a skill:</label>
+      <select id="skills" className="w-30">
+        <option value="JavaScript">JavaScript</option>
+        <option value="React">React</option>
+        <option value="CSS">CSS</option>
+        <option value="HTML">HTML</option>
+      </select>
+
+      <input
+        type="submit"
+        onSubmit={(e) => {
+          eventHandler(e);
+        }}
+      />
+    </form>
+  );
 }
