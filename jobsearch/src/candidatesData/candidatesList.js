1c82cc36990f3f6677e91f3bb7f7f20a86df4463
btxoana <beatrice.stanila@gmail.com>
2023-03-30 11:47:56 +0100

    candidates list data generated

---
+++ jobsearch/src/candidatesData/candidatesList.js
@@ -0,0 +1,68 @@
+export const candidatesList = [
+  {
+    name: "Lewis Hamilton",
+    email: "lhamiltion@gmail.com",
+    skills: ["JavaScript", "React", "CSS", "HTML", "SQL", "TailwindCSS"],
+    availability: "Two weeks",
+  },
+  {
+    name: "Shaylyn Kenyon",
+    email: "shaylynkenyon@hotmail.com",
+    skills: ["JavaScript", "Vue", "CSS", "HTML"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Katrina Randell",
+    email: "katrinarandell@gmail.com",
+    skills: ["Python", "Pandas", "JavaScript", "HTML", "CSS", "SQL"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Carolina Rexanne",
+    email: "CarolinaRexanne@gmail.com",
+    skills: ["Java", "AWS", "Azure", "HTML", "CSS", "JavaScript"],
+    availability: "Available immediately",
+  },
+  {
+    name: "June Jo-Anne",
+    email: "jo-annel@gmail.com",
+    skills: ["Python", "React", "JavaScript", "HTML", "Node", "Express"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Grant Nicholas",
+    email: "Grant Nicholas",
+    skills: ["Python", "Pandas", "JavaScript", "HTML", "TypeScript"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Phil Pacey",
+    email: "PhilPaceyl@gmail.com",
+    skills: ["Python", "SQL", "Java", "HTML", "CSS"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Brantley Corwin",
+    email: "brantleycorwin@gmail.com",
+    skills: ["Python", "Pandas", "JavaScript", "HTML", "Node", "Express"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Nic Seth",
+    email: "nicseth@gmail.com",
+    skills: ["Python", "AWS", "JavaScript", "SQL"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Philander Scottie",
+    email: "PhilanderScottie@gmail.com",
+    skills: ["Azure", "Java", "JavaScript", "HTML"],
+    availability: "Available immediately",
+  },
+  {
+    name: "Lindsey Bambi",
+    email: "lindseybambi@gmail.com",
+    skills: ["Python", "AWS", "SQL", "HTML"],
+    availability: "Available in one month",
+  },
+];
